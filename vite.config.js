6e95567881e1a8eabc0abd311a1ae9c3b6d00d6e
kondabolusaraswathi <134605077+kondabolusaraswathi@users.noreply.github.com>
2024-08-07 11:05:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+   build: {
+    outDir: 'build', // Customize the output directory
+    rollupOptions: {
+      // Customize Rollup options
+    }
+  }
 });
